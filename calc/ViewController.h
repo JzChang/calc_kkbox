22ec81971d8898b371a28c80c5e7f7667437c101
Mac <mac@Macteki-iMac.local>
2013-09-16 13:50:22 +0800

    Initial Commit

---
+++ calc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  calc
+//
+//  Created by Mac on 13/9/16.
+//  Copyright (c) 2013年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ calc/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  calc
+//
+//  Created by Mac on 13/9/16.
+//  Copyright (c) 2013年 Mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
